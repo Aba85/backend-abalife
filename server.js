cea3687447f226498ca1f8c7e6524458585c5ace
Borba <borbastyle@gmail.com>
2025-07-13 14:31:28 -0300

    Versão atualizada do backend Aba Life

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const usuariosRoutes = require('./routes/usuarios');
 app.use('/usuarios', usuariosRoutes);
 
 // Inicialização
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+}); 
