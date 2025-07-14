b5d4de45aef1556eb218413b674a1c6d8a7a4293
Borba <borbastyle@gmail.com>
2025-07-13 21:08:00 -0300

    Atualização final backend Aba Life

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
 const UsuarioController = require('../controllers/UsuarioController');
 
 router.post('/cadastrar', UsuarioController.cadastrar);
 router.post('/login', UsuarioController.login);
 
-module.exports = router;
+module.exports = router; 
