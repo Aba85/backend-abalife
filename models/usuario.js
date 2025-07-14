421f759a1b4c259660e47010de346f14e4cc44eb
Borba <borbastyle@gmail.com>
2025-07-13 22:59:18 -0300

    Atualização final backend Aba Life

--- models/usuario.js
+++ models/usuario.js
@@ -1,33 +1,40 @@
-// models/Usuario.js
-module.exports = (sequelize, DataTypes) => {
-  const Usuario = sequelize.define('Usuario', {
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    cpf: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    celular: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    senha: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    codigoIndicacao: {
-      type: DataTypes.STRING,
-      allowNull: true
-    }
-  });
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
-  return Usuario;
-};
+const Usuario = sequelize.define('Usuario', {
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  cpf: {
+    type: DataTypes.STRING(11),
+    allowNull: false,
+    unique: true,
+  },
+  celular: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  codigoIndicacao: {
+    type: DataTypes.STRING,
+    allowNull: true, // Opcional no cadastro
+  },
+  perfil: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'passageiro', // Valor padrão
+  }
+}, {
+  tableName: 'usuarios',
+});
+
+module.exports = Usuario; 
