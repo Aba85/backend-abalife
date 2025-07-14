a1362593dbd0ae56529e27575a16d242609db0e2
Borba <borbastyle@gmail.com>
2025-07-13 23:07:17 -0300

    Atualização final backend Aba Life

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,8 +1,40 @@
-const express = require('express');
-const router = express.Router();
-const UsuarioController = require('../controllers/UsuarioController');
+// models/usuario.js
 
-router.post('/cadastrar', UsuarioController.cadastrar);
-router.post('/login', UsuarioController.login);
+module.exports = (sequelize, DataTypes) => {
+  const Usuario = sequelize.define('Usuario', {
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    celular: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    codigoIndicacao: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    perfil: {
+      type: DataTypes.ENUM('passageiro', 'motorista', 'admin'),
+      defaultValue: 'passageiro',
+    },
+  }, {
+    tableName: 'usuarios',
+  });
 
-module.exports = router;
+  return Usuario;
+}; 
