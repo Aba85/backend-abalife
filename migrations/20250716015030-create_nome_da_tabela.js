9b25756b7ec7cdc0e6f95616558469172cc701b8
Borba <borbastyle@gmail.com>
2025-07-16 07:51:09 -0300

    Atualiza configurações e remove arquivos obsoletos

---
+++ migrations/20250716015030-create_nome_da_tabela.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      celular: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      endereco: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      pix: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('usuarios');
+  }
+}; 
