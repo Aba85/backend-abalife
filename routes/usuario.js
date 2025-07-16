826d510508dedfd86924fe036f3d0c3bd1881c2e
Borba <borbastyle@gmail.com>
2025-07-16 08:29:25 -0300

    Atualiza arquivo routes/usuario.js

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,25 +1,10 @@
-// backend-abalife/src/routes/usuario.js
-
 const express = require('express');
-const roteador = express.Router();
+const router = express.Router();
+
 const UsuarioController = require('../controllers/UsuarioController');
 
-// Rota para cadastrar novo usuário
-router.post ('/cadastrar', UsuarioController.cadastrar);
+// Defina suas rotas aqui
+router.post('/cadastrar', UsuarioController.cadastrar);
+// Adicione outras rotas conforme necessário
 
-// Rota para login
-router.post ('/login', UsuarioController.login);
-
-// Rota para listar todos os usuários (exemplo)
-roteador.get('/', UsuarioController.listar);
-
-// Rota para buscar usuário por id
-roteador.get('/:id', UsuarioController.buscarPorId);
-
-// Rota para atualizar o usuário
-router.put('/:id', UsuarioController.atualizar);
-
-// Rota para apagar usuário
-roteador.delete('/:id', UsuarioController.deletar);
-
-módulo.exports = roteador;
+module.exports = router; 
