0af2bbbf344b37d7477826fc2ca5ffe1c636bc26
Borba <borbastyle@gmail.com>
2025-07-16 08:15:44 -0300

    Atualiza arquivo routes/usuario.js

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,40 +1,25 @@
-// backend-abalife/src/models/usuario.js
+// backend-abalife/src/routes/usuario.js
 
-module.exports = (sequelize, DataTypes) => {
-  const Usuario = sequelize.define('Usuario', {
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    cpf: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    celular: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    senha: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    codigoIndicacao: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    perfil: {
-      type: DataTypes.ENUM('passageiro', 'motorista', 'admin'),
-      defaultValue: 'passageiro',
-    },
-  }, {
-    tableName: 'usuarios',
-  });
+const express = require('express');
+const roteador = express.Router();
+const UsuarioController = require('../controllers/UsuarioController');
 
-  return Usuario;
-}; 
+// Rota para cadastrar novo usuário
+router.post ('/cadastrar', UsuarioController.cadastrar);
+
+// Rota para login
+router.post ('/login', UsuarioController.login);
+
+// Rota para listar todos os usuários (exemplo)
+roteador.get('/', UsuarioController.listar);
+
+// Rota para buscar usuário por id
+roteador.get('/:id', UsuarioController.buscarPorId);
+
+// Rota para atualizar o usuário
+router.put('/:id', UsuarioController.atualizar);
+
+// Rota para apagar usuário
+roteador.delete('/:id', UsuarioController.deletar);
+
+módulo.exports = roteador;
