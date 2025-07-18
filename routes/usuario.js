41e9a1a3384920bf6aa97c6ad1dd16e4ad883629
Borba <borbastyle@gmail.com>
2025-07-18 07:00:43 -0300

    fix: caminho correto do UsuarioController com U maiúsculo

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const usuarioController = require('../controllers/usuarioController');
+const usuarioController = require('../controllers/UsuarioController');
 
 router.post('/usuarios/cadastrar', usuarioController.cadastrarUsuario);
 router.post('/usuarios/login', usuarioController.loginUsuario);
